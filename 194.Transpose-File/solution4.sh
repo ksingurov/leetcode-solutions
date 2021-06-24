d34e660fc1bf7aff28753c17cf77f03eb4cf7ef4
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-06-24 17:49:06 +0200

    problem-194-solution4-v2

--- 194.Transpose-File/solution4.sh
+++ 194.Transpose-File/solution4.sh
@@ -1,11 +1,21 @@
+# Script to transpose content of a txt file using awk
+# where each row has the same number of columns, and each field is separated by the ' ' character
+
+# Script Logic
+# For each line:
+#   Iterate over each field i (from 1 to number of fields NF).
+#   Store the field value $i into 2D-array with indeces = (NR, i).
+#   Then run nested loop: outer - over fields (1...NF), inner over rows (1...NR)
+#   Inside inner loop print the same field (from outer loop) for each row
+#   After each inner loop print new line
+
 awk '{
     for (i=1; i<=NF; i++) {
         content[NR, i] = $i
     }
-    max_col = NF
 }
 END {
-    for (i = 1; i <= max_col; i++) {
+    for (i = 1; i <= NF; i++) {
         for (j = 1; j <= NR; j++) {
             printf "%s ", content[j, i]
         }
