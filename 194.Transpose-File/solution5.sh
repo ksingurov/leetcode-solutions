4d075e1994abbd5dbcf0490265b5c9a05793e741
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-06-29 18:30:49 +0200

    problem-194-solution5-v2

--- 194.Transpose-File/solution5.sh
+++ 194.Transpose-File/solution5.sh
@@ -1,17 +1,13 @@
-lines=$(wc -l < file.txt | xargs)
+columns=$(head -n 1 file.txt | wc -w)
 
-columns=$(head -n 1 file.txt | wc -w | xargs)
-
-# echo "lines: $lines"
-# echo "columns: $columns"
-
-line_to_print=""
-while read -r -a line; do
-    # echo "${line[0]}"
-    if [ -z "$line_to_print" ]; then
-        line_to_print="${line[0]}"
-    else
-        line_to_print="$line_to_print ${line[0]}"
-    fi
-done < file.txt
-echo "$line_to_print"
+for i in $(seq "$columns"); do
+    line_to_print=""
+    while read -r -a line; do
+        if [ -z "$line_to_print" ]; then
+            line_to_print="${line[$i-1]}"
+        else
+            line_to_print="$line_to_print ${line[$i-1]}"
+        fi
+    done < file.txt
+    echo "$line_to_print"
+done
