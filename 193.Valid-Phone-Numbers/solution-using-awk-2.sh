8a478a0392a464a9bd54836c433746752de7d9f7
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-07-20 17:45:31 +0200

    problem-193-solution-using-awk-2-v2

--- 193.Valid-Phone-Numbers/solution-using-awk-2.sh
+++ 193.Valid-Phone-Numbers/solution-using-awk-2.sh
@@ -1,1 +1,5 @@
-awk '{ if ($0 ~ /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/) print }' file.txt
+file='file.txt'
+
+pattern='^(\\([0-9]{3}\\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$'
+
+awk -v pat="$pattern" '{ if ($0 ~ pat) print }' "$file"
