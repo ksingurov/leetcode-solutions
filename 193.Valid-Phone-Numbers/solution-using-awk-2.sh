d3a8a644b9cfa28a3c4c064a58ea642543c8f9e7
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-07-23 17:42:05 +0200

    problem-193-solution-using-awk-2-v3

--- 193.Valid-Phone-Numbers/solution-using-awk-2.sh
+++ 193.Valid-Phone-Numbers/solution-using-awk-2.sh
@@ -1,5 +1,7 @@
 file='file.txt'
 
+# Given that the variable is defined in bash and then passed to awk with "$"
+# extra backslash is required to keep backslash inside awk string
 pattern='^(\\([0-9]{3}\\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$'
 
 awk -v pat="$pattern" '{ if ($0 ~ pat) print }' "$file"
