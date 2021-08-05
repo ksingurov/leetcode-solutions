783d277661ea7a7f06fe8e50623e7fc307370560
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-08-05 18:10:55 +0200

    problem-193-solution-using-grep-v4

--- 193.Valid-Phone-Numbers/solution-using-grep.sh
+++ 193.Valid-Phone-Numbers/solution-using-grep.sh
@@ -2,4 +2,6 @@ file='file.txt'
 
 pattern='^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$'
 
+# -E enables extended regular expressions (ERE)
+# and allow to use special regex syntax which are used in the pattern
 grep -E "$pattern" "$file"
