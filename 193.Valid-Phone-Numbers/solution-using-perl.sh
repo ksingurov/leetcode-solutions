c84ba23dcdba96b15f07b7477846ec9761f04792
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-08-06 17:56:30 +0200

    problem-193-solution-using-perl-v2

--- 193.Valid-Phone-Numbers/solution-using-perl.sh
+++ 193.Valid-Phone-Numbers/solution-using-perl.sh
@@ -1,1 +1,5 @@
-perl -ne 'print if /^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$/' file.txt
+file='file.txt'
+
+# -n reads the file line by line
+# -e allow to execute provided string
+perl -ne 'print if /^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$/' "$file"
