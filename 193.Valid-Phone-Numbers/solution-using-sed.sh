3e2e4470cd29cf9a2dd981e8086b6ca3f79078d4
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-08-18 17:16:25 +0200

    problem-193-solution-using-sed-v2

--- 193.Valid-Phone-Numbers/solution-using-sed.sh
+++ 193.Valid-Phone-Numbers/solution-using-sed.sh
@@ -1,1 +1,5 @@
-sed -nE '/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/p' file.txt
+file='file.txt'
+
+# -n suppresses automatic printing of every line
+# -E enables extended regular expressions (ERE), similar to grep
+sed -nE '/^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/p' "$file"
