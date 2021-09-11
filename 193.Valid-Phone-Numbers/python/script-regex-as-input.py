178785b133984ba658577a153ce853a81d0f1fb8
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-09-11 11:08:35 +0200

    problem-193-add-script-regex-as-input

---
+++ 193.Valid-Phone-Numbers/python/script-regex-as-input.py
@@ -0,0 +1,18 @@
+import sys
+import re
+
+file_name = sys.argv[1]
+
+pattern = r"^\(?\d{3}\)?( |-)\d{3}-\d{4}$"
+
+with open(file_name, "r") as file:
+    for line in file:
+        match = re.search(pattern, line)
+        if match:
+            print(line.rstrip("\n"))
+    
+    # for line in file:
+    #     if re.search(pattern, line):
+            # print(line.rstrip("\n"))
+
+    # [print(line.rstrip("\n")) for line in file if re.search(pattern, line)]
