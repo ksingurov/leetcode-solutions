fd22c1f0e383c5cc7bfbc140662f204a80962a25
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-09-11 11:02:29 +0200

    problem-193-script-examples-as-input-v3

--- 193.Valid-Phone-Numbers/python/script-examples-as-input.py
+++ 193.Valid-Phone-Numbers/python/script-examples-as-input.py
@@ -2,7 +2,7 @@ import sys
 import re
 from typing import Match
 
-# function to return all phone numbers from the file
+# function to retrive phone numbers from the file
 # return None if file doesn't exists
 def get_phone_numbers(file_name: str) -> list[str] | None:
     try:
@@ -22,7 +22,6 @@ def digits_sequence(match: Match[str]) -> str:
 # function to substitute all alphanumeric sequences with string "\d{n}", where n is the lenght of the sequence
 # uses function defined earlier as an repl parameters in re.sub
 def convert_to_pattern(phone_example) -> str:
-    # return "^" + re.sub(pattern=r"[A-Za-z0-9_]+", repl=digits_sequence, string=phone_example) + "$"
     result = re.sub(pattern=r"[A-Za-z0-9_]+", repl=digits_sequence, string=phone_example)
     return "^" + re.escape(result) + "$"
 
@@ -48,16 +47,12 @@ def main():
 
     #convert examples to patterns; set is used to avoid identical patterns
     patterns = {convert_to_pattern(example) for example in examples}
-    # print(f"[DEBUG]: patterns -> {patterns}")
 
     # get valid phone numbers
     valid_phone_numbers = []
     for pat in patterns:
         valid_phone_numbers += get_valid_phone_numbers(phone_numbers, pat)
-        # print(f"[DEBUG]: pat -> {repr(pat)}")
-        # print(f"[DEBUG]: valid_phone_numbers -> {valid_phone_numbers}")
     print("\n".join(valid_phone_numbers))
 
-# entry-point
 if __name__ == "__main__":
     main()
