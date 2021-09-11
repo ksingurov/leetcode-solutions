65ce319333cc728074cee85990bdb2c4ff8568a0
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-09-11 11:31:49 +0200

    problem-193-script-regex-as-input-v5

--- 193.Valid-Phone-Numbers/python/script-regex-as-input.py
+++ 193.Valid-Phone-Numbers/python/script-regex-as-input.py
@@ -2,17 +2,22 @@ import sys
 import re
 
 file_name = sys.argv[1]
+pattern = sys.argv[2]
+# print(pattern)
 
-pattern = r"^\(?\d{3}\)?( |-)\d{3}-\d{4}$"
+# pattern = r"^\(?\d{3}\)?( |-)\d{3}-\d{4}$"
 
 with open(file_name, "r") as file:
-    for line in file:
-        match = re.search(pattern, line)
-        if match:
-            print(line.rstrip("\n"))
-    
+    # for line in file:
+    #     match = re.search(pattern, line)
+    #     if match:
+    #         print(line.rstrip("\n"))
+
     # for line in file:
     #     if re.search(pattern, line):
             # print(line.rstrip("\n"))
 
     # [print(line.rstrip("\n")) for line in file if re.search(pattern, line)]
+    lines_to_print = [line.rstrip("\n") for line in file if re.search(pattern, line)]
+
+print("\n".join(lines_to_print))
