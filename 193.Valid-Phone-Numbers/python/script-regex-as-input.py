55dd79fcc63e938dbbacbc038a732e03c2183045
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-09-14 18:30:13 +0200

    problem-193-script-regex-as-input-v8

--- 193.Valid-Phone-Numbers/python/script-regex-as-input.py
+++ 193.Valid-Phone-Numbers/python/script-regex-as-input.py
@@ -1,7 +1,17 @@
 import sys
 import re
 
-# define function which reads lines within a file and checks if they match provided pattern
+# function to validate provided pattern
+def is_valid_regex(pattern: str) -> bool:
+    print(f"[DEBUG] Testing pattern: {repr(pattern)}")
+    try:
+        re.compile(pattern)
+        return True, "OK"
+    except re.error as e:
+        print(f"[DEBUG] Regex error: {e}")
+        return False, f"Invalid regex: {e}"
+
+# function which reads lines within a file and checks if they match provided pattern
 def valid_phone_numbers(file_name: str, pattern: str) -> str:
     with open(file_name, "r") as file:
         lines_to_print = [line.rstrip("\n") for line in file if re.search(pattern, line)]
@@ -12,6 +22,15 @@ if __name__ == "__main__":
     # get parameters from command line
     file_name = sys.argv[1]
     pattern = sys.argv[2]
+
+    # check pattern
+    is_valid, msg = is_valid_regex(pattern)
+    if not is_valid:
+        print(f"Error -> {msg}")
+        regex_syntax_link = "https://docs.python.org/3/library/re.html#regular-expression-syntax"
+        print(f"Refer to regex syntax here: {regex_syntax_link}")
+        sys.exit(1)
+    
     # pass parameters to the functions and print the result
     res = valid_phone_numbers(file_name, pattern)
     print(res)
