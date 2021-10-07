a1e1147269f24566af8c7454190283157f28749f
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-10-07 17:05:01 +0200

    problem-193-script-regex-pattern-as-input-v5

--- 193.Valid-Phone-Numbers/perl/script-regex-pattern-as-input.pl
+++ 193.Valid-Phone-Numbers/perl/script-regex-pattern-as-input.pl
@@ -3,20 +3,25 @@
 
 my $pattern = shift @ARGV;
 my $regex;
+my $file_name = shift @ARGV;
 
-# unless (defined $pattern) {
-#     die "Error: regex-pattern was not provided\nUsage: $0 <regex-pattern> <file-name>\n";
-# }
+unless (defined $pattern) {
+    die "Error: no regex-pattern was provided\nUsage: $0 <regex-pattern> <file-name>\n";
+}
 
 eval {$regex = qr/$pattern/};
 if ($@) {
     die "Error: invalid regex pattern:\n $@";
     }
 
-# print "$pattern\n";
-# print $regex;
+unless (defined $file_name) {
+    die "Error: no file name was provided\nUsage: $0 <regex-pattern> <file-name>\n";
+}
+
+unless (-e $file_name) {
+    die "Error: file $file_name doesn't exist\nUsage: $0 <regex-pattern> <file-name>\n";
+}
 
 while (<>) {
-    # print
     print if /$regex/;
 }
