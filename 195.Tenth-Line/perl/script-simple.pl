41bc46d089e3cdeddb2153d1b65018c6c0e35482
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-10-12 20:01:07 +0200

    problem-195-script-simple-v3

--- 195.Tenth-Line/perl/script-simple.pl
+++ 195.Tenth-Line/perl/script-simple.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
+# Usage: ./script.pl [filename]
 
 # loop of the lines of an input file
 # $. is a variable holding current line number
 while (<>) {
     print if $. == 10;
+    last if $. == 10; # stops after tenth line
 }
