df1096a55d72442760f7b700e529e9cc78deacc1
ksingurov <35699996+ksingurov@users.noreply.github.com>
2021-10-21 19:36:40 +0200

    problem-195-script-with-argv-v2

--- 195.Tenth-Line/perl/script-with-argv.pl
+++ 195.Tenth-Line/perl/script-with-argv.pl
@@ -1,9 +1,14 @@
 #!/usr/bin/perl
-# Usage: ./script.pl [line-number] [file-name]
+# Usage: ./script.pl <line-number> <file-name>
 
 # get line number from the command line input
 # ARGV is an array which stores parameters passed to the script
 my $n_line_to_print = $ARGV[0];
+
+# check if line number was passed as a parameter
+unless (defined $n_line_to_print) {
+    die "Error: no line number was provided.\nUsage: $0 <line-number> <filename>\n";
+}
 
 # loop of the lines of an input file
 # $. is a variable holding current line number
