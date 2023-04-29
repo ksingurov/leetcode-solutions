b8da21f8a457f32e1ac1443dea17cd9775718fe1
ksingurov <35699996+ksingurov@users.noreply.github.com>
2023-04-29 19:27:39 +0200

    add-problem-17-comments

--- 17.Letter-Combinations-of-a-Phone-Number/solution.py
+++ 17.Letter-Combinations-of-a-Phone-Number/solution.py
@@ -2,6 +2,7 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # Mapping of digits to letters on a phone keypad
         phone_numbers = {
             "2": "abc",
             "3": "def",
@@ -13,6 +14,14 @@ class Solution:
             "9": "wxyz",
         }
 
+        # Recursive function to generate combinations
+        # Logic:
+        # Input is a list of strings, and an index of the character (digit) to replace with letters
+        # strings are supposed to be aplanumeric: left part before index are letters already chosen,
+        # and right part after index are digits still present and to be replaced
+        # The function generates all combinations for the given index
+        # by replacing the digit at the index with letters from the phone_numbers mapping,
+        # Then it recursively calls itself and passes the new combinations and the next index.
         def generate_combinations(combinmations, index=0):
             if index == len(combinmations[0]):
                 return combinmations
@@ -21,6 +30,7 @@ class Solution:
                 new_combinations += [c[:index] + letter + c[index + 1:] for letter in phone_numbers[c[index]]]
             return generate_combinations(new_combinations, index + 1)
 
+        # call the recursive function only if the input string is not empty
         if len(digits) > 0:
             return generate_combinations([digits])
         return []
