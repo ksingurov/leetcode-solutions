b3b38c52ad670b479173a8a4168f370cb031c8c9
ksingurov <35699996+ksingurov@users.noreply.github.com>
2023-04-30 19:30:42 +0200

    add-problem-20-solution-2-comments

--- 20.Valid-Parentheses/solution2.py
+++ 20.Valid-Parentheses/solution2.py
@@ -1,3 +1,14 @@
+# Efficient stack based solution
+# Logic:
+# Iterate over characters in the string, and check folllowing condition:
+# - if character is an opening bracket ("if")
+#   -> add it to the stack
+# - if it is not, and stack is empty or last paranthesis in the stack doesn't match the current ("elif")
+#   -> invalid sequence of parentheses - end the cycle and return False
+# - otherwise ("else" = i.e. if parentheses matched)
+#   -> remove last opening parenthesis
+
+
 class Solution:
     def isValid(self, s: str) -> bool:
         mapping = {')': '(', ']': '[', '}': '{'}
