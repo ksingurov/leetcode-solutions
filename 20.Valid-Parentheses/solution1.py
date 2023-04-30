89c12ea726e806ab395c4f0d8d9c3f06b1fa1bd7
ksingurov <35699996+ksingurov@users.noreply.github.com>
2023-04-30 19:24:17 +0200

    add-problem-20-solution-1-comments

--- 20.Valid-Parentheses/solution1.py
+++ 20.Valid-Parentheses/solution1.py
@@ -1,3 +1,9 @@
+# Elegant solution with recursion and regex.
+# Logic:
+# Recursively remove pairs of valid parentheses, brackets, or braces from the string.
+# If the string becomes empty, it means all pairs were valid, and we return True.
+# If no pairs can be removed and the string is not empty, we return False.
+
 import re
 
 class Solution:
