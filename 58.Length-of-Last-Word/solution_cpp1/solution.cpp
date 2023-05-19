788c0d93fa7d9f60298c8ae6b2f367abf816e640
ksingurov <35699996+ksingurov@users.noreply.github.com>
2023-05-19 10:11:17 +0200

    add-problem-58-solution-c-0

---
+++ 58.Length-of-Last-Word/solution_cpp1/solution.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+class Solution {
+    public:
+        int lengthOfLastWord(const std::string& s) {
+            int i = s.length() - 1;
+            while (i >= 0 && s[i] == ' ') --i;
+            int j = i;
+            while (j >= 0 && s[j] != ' ') --j;
+            return i - j;
+        }
+    };
