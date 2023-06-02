83eae9b94e058fe9a3232808ce047ef8d8ef889b
ksingurov <35699996+ksingurov@users.noreply.github.com>
2023-06-02 19:39:51 +0200

    problem-3-python-tests-simple

---
+++ 3.Longest-Substring-Without-Repeating-Characters/solution_python/1-optimized-sliding-window/test_solution_assert_one_by_one.py
@@ -0,0 +1,6 @@
+from solution import Solution
+
+sol = Solution()
+assert sol.lengthOfLongestSubstring("abcabcbb") == 3
+assert sol.lengthOfLongestSubstring("bbbbb") == 1
+assert sol.lengthOfLongestSubstring("pwwkew") ==  3
