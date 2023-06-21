9c2bf9c9eeeaf62a02d329318319ee5428567ae8
ksingurov <35699996+ksingurov@users.noreply.github.com>
2023-06-21 09:35:16 +0200

    problem-5-python-brute-force-fix

--- 5.Longest-Palindromic-Substring/0-brute-force/solution.py
+++ 5.Longest-Palindromic-Substring/0-brute-force/solution.py
@@ -5,17 +5,16 @@ class Solution:
             return 0
         
         p_substring = s[1]
-        for l_substring in range(1, l_string + 1):
-            for i in range(l_string - l_substring):
+        for l_substring in range(2, l_string + 1):
+            for i in range(l_string + 1 - l_substring):
                 substring = s[i:i + l_substring]
-                if substring == substring[::-1]:
+                if substring == substring[::-1] and len(substring) > len(p_substring):
                     p_substring = substring
-                    break
         return p_substring
-
+    
 
 if __name__ == "__main__":
     sol = Solution()
-    s = "abba"
+    s = "bbabbcbbax"
     res = sol.longestPalindrome(s)
     print(res)
