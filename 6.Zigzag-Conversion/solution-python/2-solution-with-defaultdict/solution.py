f299c4d47cd195d3f872affef316bc09ad5b4515
ksingurov <35699996+ksingurov@users.noreply.github.com>
2023-07-08 11:23:57 +0200

    problem-6-python-defauldict-2

--- 6.Zigzag-Conversion/solution-python/2-solution-with-defaultdict/solution.py
+++ 6.Zigzag-Conversion/solution-python/2-solution-with-defaultdict/solution.py
@@ -1,33 +1,21 @@
 # use defaultdict instead of list of typles
 # in this case it is appended in a "right" order while looping over input string
+# actually we don't need x coordinate since letters are already ordered in rows accordingly
 
 from collections import defaultdict
 
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         zigzag = defaultdict(list)
-        movements = {'down': (0, 1), 'up': (1, -1)}
-        
-        x, y = 1, 1
-        next_move = movements['down']
+
+        y = 1
+        next_move = 1
         for letter in s:
-            zigzag[y].append((x, letter))
-            x = x + next_move[0]
-            y = y + next_move[1]
+            zigzag[y].append(letter)
+            y += next_move
             if y == numRows:
-                next_move = movements['up']
+                next_move = -1
             elif y == 1:
-                next_move = movements['down']
+                next_move = 1
 
-        new_string = "".join([item[1] for v in zigzag.values() for item in v])
-        return new_string
-
-if __name__ == "__main__":
-    s = "PAYPALISHIRING"
-    numRows = 3
-    exp = "PAHNAPLSIIGYIR"
-    sol = Solution()
-    res = sol.convert(s=s, numRows=numRows)
-    print(res == exp)
-    # print(res)
-    # help(defaultdict)
+        return "".join([item for v in zigzag.values() for item in v])
