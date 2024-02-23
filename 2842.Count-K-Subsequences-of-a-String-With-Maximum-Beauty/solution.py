3be7e379092bcd52e3c2b56d2152d80f910d624a
ksingurov <35699996+ksingurov@users.noreply.github.com>
2024-02-23 17:24:35 +0100

    problem-2842-solution-v3

--- 2842.Count-K-Subsequences-of-a-String-With-Maximum-Beauty/solution.py
+++ 2842.Count-K-Subsequences-of-a-String-With-Maximum-Beauty/solution.py
@@ -1,4 +1,6 @@
-# TODO: check soluton for string with all characters the same
+# 4.06.2025
+# wrong answer, but the idea is correct
+# instead of most common in the end we need to return the count of the maximum beauty
 
 import collections
 import itertools
@@ -14,14 +16,17 @@ class Solution:
         # element of k_subsequences is a list of characters
         k_subsequences = [sub for sub in all_subsequences if collections.Counter(sub).most_common(1)[0][1] == 1]
 
-        # list of all beauties of the string s
-        beauties = []
-        # for each k-subsequence calculate sum of occurances in the original string s
-        # since elemenets in k_subsequences in a list, just list of occurances with list comprehensions, and the sum it
-        for k_sub in k_subsequences:
-            beauties += [sum([occurances[element] for element in k_sub])]
+        # check if there are no subsequences
+        if len(k_subsequences) == 0:
+            return 0
+        else:
+            # list of all beauties of the string s
+            beauties = []
+            # for each k-subsequence calculate sum of occurances in the original string s
+            # since elemenets in k_subsequences in a list, just list of occurances with list comprehensions, and the sum it
+            for k_sub in k_subsequences:
+                beauties += [sum([occurances[element] for element in k_sub])]
 
-        # return the counter for most common element in the list of beauties
-        return collections.Counter(beauties).most_common(1)[0][1]
-
-        
+            # return the counter for most common element in the list of beauties
+            return collections.Counter(beauties).most_common(1)[0][1]
+    
