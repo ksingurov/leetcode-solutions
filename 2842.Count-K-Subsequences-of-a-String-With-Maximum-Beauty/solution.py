0b921e18f9daaa83a808b112b28e96ef45e3194b
ksingurov <35699996+ksingurov@users.noreply.github.com>
2024-02-25 18:22:51 +0100

    problem-2842-solution-v6

--- 2842.Count-K-Subsequences-of-a-String-With-Maximum-Beauty/solution.py
+++ 2842.Count-K-Subsequences-of-a-String-With-Maximum-Beauty/solution.py
@@ -1,36 +1,26 @@
-
-# to imporove performance, we check if the string s has at least k different characters
-# and if not, return 0 immediately
-# prevuois version of the code was checking len of k_subsequences
-
 import collections
-import itertools
+import math
 
 class Solution:
     def countKSubsequencesWithMaxBeauty(self, s: str, k: int) -> int:
         # all f(x), i.e. number of times each character occurs in the string s
         occurances = collections.Counter(s) # dict {'character': occurance}
-
-        # if there are less than k different characters in the string s, return 0
-        # i.e. we cannot form a k-subsequence with unique characters
         if len(occurances) < k:
             return 0
-        
-        # all possible subsequences of length k
-        all_subsequences = itertools.combinations(list(s), k)
-        # to get k-subsequences, keep only those with most common occurance equal to 1
-        # element of k_subsequences is a list of characters
-        k_subsequences = [sub for sub in all_subsequences if collections.Counter(sub).most_common(1)[0][1] == 1]
 
-        # list of all beauties of the string s
-        beauties = []
-        # for each k-subsequence calculate sum of occurances in the original string s
-        # since elemenets in k_subsequences in a list, just list of occurances with list comprehensions, and the sum it
-        for k_sub in k_subsequences:
-            beauties += [sum([occurances[element] for element in k_sub])]
+        # we need only k-subsequences which would have maximum possible beauty
+        # i.e. those k-subsequences should contain unique charachters with biggest beaities, i.e. frequencies
+        # thus let's find k most frequent chararters
+        sorted_occurances = sorted(occurances.items(), key=lambda item: item[1], reverse=True)
+        k_occurances = sorted_occurances[:k]
 
-        # return the counter for biggest element in the list of beauties
-        c = collections.Counter(beauties)
-        m = max(c.keys())
-        return c[m]
-    
+        # it is possible that several charachters might have the same frequency
+        # it is not a problem if all of them among characters within first elements of sorted_occurances
+        # however if k-th and k+1-th element have the same frequency
+        # then there are several combinations of unique characters based on which we can compose k-subsequence
+        kth_occurance = k_occurances[-1][1]
+        n_kth_to_select = len([item for item in k_occurances if item[1] == kth_occurance])
+        n_kth_total = len([item for item in sorted_occurances if item[1] == kth_occurance])
+
+        # so now we just need to calculate number of combinations with k_occurances
+        return math.prod([item[1] for item in k_occurances]) * math.comb(n_kth_total, n_kth_to_select)
