8b44d3a6666f46fee5608904a43975834f30538e
ksingurov <35699996+ksingurov@users.noreply.github.com>
2024-03-06 18:40:05 +0100

    problem-2928-solution-v2

--- 2928.Distribute-Candies-Among-Children-I/solution.py
+++ 2928.Distribute-Candies-Among-Children-I/solution.py
@@ -1,11 +1,10 @@
-# brute force solution
+# a bit less brute force solution
 
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
         count = 0
         for i in range(min(limit, n) + 1):
-            for j in range(min(limit, n - i) + 1):
-                if n - i - j <= limit:
-                    # print("1st: ", i , "\t2nd: ", j, "\t3rd: ", n - i - j)
-                    count += 1
+            for j in range(max(0, n - i - limit), min(limit, n - i) + 1):
+                # print("1st: ", i , "\t2nd: ", j, "\t3rd: ", n - i - j)
+                count += 1
         return count
