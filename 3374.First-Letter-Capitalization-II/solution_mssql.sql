c2881b70930e62c5465580906e8b00db902cf8ff
ksingurov <35699996+ksingurov@users.noreply.github.com>
2024-12-05 18:57:56 +0100

    problem-3374-solution-mssql-v2

--- 3374.First-Letter-Capitalization-II/solution_mssql.sql
+++ 3374.First-Letter-Capitalization-II/solution_mssql.sql
@@ -20,6 +20,27 @@ WITH
         FROM Space_positions
         WHERE CHARINDEX(' ', content_text, position + 1) > 0
     )
+    , hyphen_positions AS (
+        -- Anchor: find first occurrence per content_id
+        SELECT
+            content_id
+            , content_text
+            , CHARINDEX('-', content_text) AS position
+            , 'hyphen' AS separator_type
+        FROM user_content
+        WHERE CHARINDEX('-', content_text) > 0
+
+        UNION ALL
+
+        -- Recursive: find next occurrence starting after previous position
+        SELECT
+            content_id
+            , content_text
+            , CHARINDEX('-', content_text, position + 1) AS position
+            , 'hyphen' AS separator_type
+        FROM Space_positions
+        WHERE CHARINDEX('-', content_text, position + 1) > 0
+    )
 
 SELECT
     content_id
@@ -27,3 +48,21 @@ SELECT
     , position
     , separator_type
 FROM space_positions
+
+UNION
+
+SELECT
+    content_id
+    , content_text
+    , position
+    , separator_type
+FROM hyphen_positions
+
+UNION
+
+SELECT
+    content_id
+    , content_text
+    , 1 AS position
+    , NULL AS separator_type
+FROM user_content
