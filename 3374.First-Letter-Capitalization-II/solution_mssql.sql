ce21e3f8b615d4efa48d3158acb3e1abebc9d5c0
ksingurov <35699996+ksingurov@users.noreply.github.com>
2024-12-09 18:41:21 +0100

    problem-3374-solution-mssql-v3

--- 3374.First-Letter-Capitalization-II/solution_mssql.sql
+++ 3374.First-Letter-Capitalization-II/solution_mssql.sql
@@ -41,28 +41,96 @@ WITH
         FROM Space_positions
         WHERE CHARINDEX('-', content_text, position + 1) > 0
     )
+    , first_letter_positions AS (
+        SELECT
+            content_id
+            , content_text
+            , position + 1 AS position
+            , separator_type
+        FROM space_positions
+        -- OPTION (MAXRECURSION 0);
+
+        UNION
+
+        SELECT
+            content_id
+            , content_text
+            , position + 1 AS position
+            , separator_type
+        FROM hyphen_positions
+
+        UNION
+
+        SELECT
+            content_id
+            , content_text
+            , 1 AS position
+            , NULL AS separator_type
+        FROM user_content
+        -- OPTION (MAXRECURSION 0)
+    )
+    , first_letter_positions_order AS (
+        SELECT
+            content_id
+            , LOWER(content_text) AS content_text
+            , position
+            , ROW_NUMBER() OVER (PARTITION BY content_id ORDER BY position) AS position_order
+            , separator_type            
+        FROM first_letter_positions
+    )
+    -- Final recursive CTE
+    , converted_text AS (
+        -- Anchor
+        SELECT
+            content_id
+            , content_text AS original_text
+            , STUFF(
+                content_text, 
+                position,
+                1,
+                UPPER(SUBSTRING(content_text, position, 1))
+            ) AS converted_text
+            , position_order
+        FROM first_letter_positions_order
+        WHERE position_order = 1      
+        UNION ALL
+        -- Recursive
+        SELECT
+            converted.content_id
+            , converted.original_text
+            , STUFF(
+                converted.converted_text, 
+                original.position,
+                1,
+                UPPER(SUBSTRING(converted.converted_text, original.position, 1))
+            ) AS converted_text
+            , original.position_order
+        FROM converted_text converted
+        INNER JOIN first_letter_positions_order original
+            ON converted.content_id = original.content_id
+                AND converted.position_order + 1 = original.position_order
+    )
+    , converted_text_with_row_n AS (
+        SELECT
+            content_id
+            , original_text
+            , converted_text
+            , ROW_NUMBER() OVER (PARTITION BY content_id ORDER BY position_order DESC) AS row_n
+        FROM converted_text       
+    )
+
+-- SELECT * FROM converted_text
+-- ORDER BY content_id, position_order
+
+
+-- SELECT
+--     *
+--     , ROW_NUMBER() OVER (PARTITION BY content_id ORDER BY position_order DESC) AS row_n
+-- FROM converted_text
 
 SELECT
     content_id
-    , content_text
-    , position
-    , separator_type
-FROM space_positions
-
-UNION
-
-SELECT
-    content_id
-    , content_text
-    , position
-    , separator_type
-FROM hyphen_positions
-
-UNION
-
-SELECT
-    content_id
-    , content_text
-    , 1 AS position
-    , NULL AS separator_type
-FROM user_content
+    , original_text
+    , converted_text
+FROM converted_text_with_row_n
+WHERE row_n = 1
