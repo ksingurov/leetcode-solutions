50104547e4859cb1690aa8d2516cce0942815335
ksingurov <35699996+ksingurov@users.noreply.github.com>
2025-03-18 18:49:43 +0900

    problem-3482-play-with-recursive-cte

---
+++ 3482.Analyze-Organization-Hierarchy/playing_with_resursive_cte.sql
@@ -0,0 +1,23 @@
+WITH cte_levels AS (
+    SELECT
+        employee_id
+        , employee_name
+        , manager_id
+        , 1 AS level
+    FROM Employees
+    WHERE manager_id IS NULL
+
+    UNION ALL
+
+    SELECT
+        e.employee_id
+        , e.employee_name
+        , e.manager_id
+        , l.level + 1 AS level
+    FROM Employees e
+    INNER JOIN cte_levels l ON e.manager_id = l.employee_id
+)
+
+SELECT *
+FROM cte_levels
+ORDER BY level
