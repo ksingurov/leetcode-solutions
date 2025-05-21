bbe53049b843199fe5cddfbb8822b007545b311a
ksingurov <35699996+ksingurov@users.noreply.github.com>
2025-05-21 13:01:26 +0200

    add-comments-to-problem-1321-solutions

--- 1321.Restaurant-Growth/solution_mssql2.sql
+++ 1321.Restaurant-Growth/solution_mssql2.sql
@@ -1,3 +1,18 @@
+-- second solution
+-- AVG could be calculated as a SUM / COUNT and making the query more efficient
+
+-- apparently, the it is not necessary to calculate the aggregates for the first 6 days
+-- which could make the query even more efficient
+-- is it possible to skip the first 6 days? - no
+
+-- important note / limitations of the solution
+-- also ROWS BETWEEN is used since it is said "here will be at least one customer every day"
+-- however, it is not guaranteed that there are customer every day in reality
+-- so it is better to use RANGE BETWEEN
+-- also some dates could be missing in the data, but moving average should be still calculated for those dates
+-- so, we need to create a proxy table with all dates in the range between min and max dates
+-- and join aggretated sums per day to that table before calculating moving average
+
 WITH 
     amount_per_day AS (
         SELECT
