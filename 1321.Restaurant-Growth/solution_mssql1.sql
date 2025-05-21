ca30eb947bd2192cfa07a8730794a6154ef249ae
ksingurov <35699996+ksingurov@users.noreply.github.com>
2025-05-21 12:51:33 +0200

    add-problem-1321-solution1

---
+++ 1321.Restaurant-Growth/solution_mssql1.sql
@@ -0,0 +1,24 @@
+WITH 
+    amount_per_day AS (
+        SELECT
+            visited_on
+            , CAST(SUM(amount) AS FLOAT) AS amount
+        FROM Customer
+        GROUP BY visited_on
+    )
+    , aggregates AS (
+        SELECT
+            visited_on
+            , SUM(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount
+            , AVG(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS average_amount
+            , COUNT(*) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS days_count
+        FROM amount_per_day
+    )
+
+SELECT
+    visited_on
+    , amount
+    , ROUND(average_amount, 2) AS average_amount
+FROM aggregates
+WHERE days_count = 7
+ORDER BY visited_on
