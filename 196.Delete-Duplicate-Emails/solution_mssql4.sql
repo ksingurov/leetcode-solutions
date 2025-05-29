ba3e0a33bb7c4a90ec930154042b5a858ec6d944
ksingurov <35699996+ksingurov@users.noreply.github.com>
2025-05-29 12:55:12 +0200

    add-problem-196-solutions-4-5

---
+++ 196.Delete-Duplicate-Emails/solution_mssql4.sql
@@ -0,0 +1,15 @@
+-- with IN inted of NOT IN
+-- beats ~28%   
+
+WITH
+    row_n AS (
+        SELECT
+            id
+            , ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS row_n
+        FROM Person
+    )
+
+DELETE FROM Person
+WHERE id IN (
+    SELECT id FROM row_n WHERE row_n > 1
+)
