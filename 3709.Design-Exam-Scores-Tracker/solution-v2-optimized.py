977a30fd3d3c8004c3ff30db8137ed4bb6c791fb
ksingurov <35699996+ksingurov@users.noreply.github.com>
2025-11-12 09:09:35 +0800

    3809-optimized-solution-adjusted

--- 3709.Design-Exam-Scores-Tracker/solution-v2-optimized.py
+++ 3709.Design-Exam-Scores-Tracker/solution-v2-optimized.py
@@ -1,4 +1,5 @@
 from bisect import bisect_right
+from typing import Optional
 
 
 class ExamTracker:
@@ -12,30 +13,37 @@ class ExamTracker:
         self.times.append(time)
         self.all_cum_scores.append(last_cum + score)
 
-    def get_left_sum(self, t: int):
+    def _get_left_sum(self, t: int):
         index = bisect_right(self.times, t) - 1
         return self.all_cum_scores[index] if index >= 0 else 0
 
     def totalScore(self, startTime: int, endTime: int) -> int:
-        return self.get_left_sum(endTime) - self.get_left_sum(startTime - 1)
-
+        return self._get_left_sum(endTime) - self._get_left_sum(startTime - 1)
 
 
 if __name__ == "__main__":
     def tester(input: list):
         obj = ExamTracker()
+        actual_results: list[Optional[int]] = [None]
         for action, data in zip(input[0][1:], input[1][1:]):
-            print(f"method: {action}, data: {data}")
+            # print(f"method: {action}, data: {data}")
             if action == "record":
                 obj.record(*data)
+                actual_results.append(None)
             else:
-                print(f"{data}: {obj.totalScore(*data)}")
+                # print(f"{data}: {obj.totalScore(*data)}")
+                actual_results.append(obj.totalScore(*data))
+        return actual_results
 
     input = [
         ["ExamTracker", "record", "totalScore", "record", "totalScore", "totalScore", "totalScore", "totalScore"], 
         [[], [1, 98], [1, 1], [5, 99], [1, 3], [1, 5], [3, 4], [2, 5]]
     ]
 
-    expected = [None, None, 98, None, 98, 197, 0, 99]
+    expected_results = [None, None, 98, None, 98, 197, 0, 99]
 
-    tester(input=input)
+    actual_results = tester(input=input)
+
+    assert expected_results == actual_results, "fail"
+
+
